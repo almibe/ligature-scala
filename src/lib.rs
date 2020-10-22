d2f4253c737e81629002fc402bc0063fbfc00461
berry3 <berry3@indiana.edu>
2020-10-22 19:44:19 -0400

    fixing some types

--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,26 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait Slonky {
-    async fn read<T, E>(&self, f: dyn FnOnce(dyn ReadTx) -> Result<T, E>) -> Result<T, E>;
-    async fn write<E>(&self, f: dyn FnOnce(dyn WriteTx) -> Result<(), E>) -> Result<(), E>;
+    async fn read<T, E>(&self, f: &dyn FnOnce(dyn ReadTx) -> Result<T, E>) -> Result<T, E>;
+    async fn write<E>(&self, f: &dyn FnOnce(dyn WriteTx) -> Result<(), E>) -> Result<(), E>;
 }
 
 #[async_trait]
 pub trait ReadTx {
     async fn key_exists(&self, key: &[u8]) -> bool;
     async fn prefix_exists(&self, prefix: &[u8]) -> bool;
-    async fn get(&self, key: &[u8]) -> Option<&[u8]>;
-    async fn prefix_scan(&self, prefix: &[u8]) -> dyn Stream<Item=(&[u8], &[u8])>;
-    async fn range_scan(&self, from: &[u8], to: &[u8]) -> dyn Stream<Item=(&[u8], &[u8])>;
-    async fn scan_all(&self) -> dyn Stream<Item=(&[u8], &[u8])>;
+    async fn get(&self, key: &[u8]) -> Option<Vec<u8>>;
+    async fn prefix_scan(&self, prefix: &[u8]) -> dyn Stream<Item=(Vec<u8>, Vec<u8>)>;
+    async fn range_scan(&self, from: &[u8], to: &[u8]) -> dyn Stream<Item=(Vec<u8>, Vec<u8>)>;
+    async fn scan_all(&self) -> dyn Stream<Item=(Vec<u8>, Vec<u8>)>;
 }
 
 #[async_trait]
 pub trait WriteTx {
     async fn key_exists(&self, key: &[u8]) -> bool;
     async fn prefix_exists(&self, prefix: &[u8]) -> bool;
-    async fn get(&self, key: &[u8]) -> Option<&[u8]>;
-    async fn put(&self, key: &[u8], value: &[u8]) -> (&[u8], &[u8]);
-    async fn remove(&self, key: &[u8]) -> (&[u8], &[u8]);
+    async fn get(&self, key: &[u8]) -> Option<Vec<u8>>;
+    async fn put(&self, key: &[u8], value: &[u8]) -> (Vec<u8>, Vec<u8>);
+    async fn remove(&self, key: &[u8]) -> (Vec<u8>, Vec<u8>);
     async fn cancel(&self);
 }
