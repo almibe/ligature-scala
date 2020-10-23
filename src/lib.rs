78f09982e39695758ae5d6239398aecd48ed8fb6
berry3 <berry3@indiana.edu>
2020-10-22 23:29:01 -0400

    adding some extra boxes

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,17 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 pub trait Slonky {
-    fn read<T, E>(&self, f: Box<dyn Fn(dyn ReadTx) -> Result<T, E>  + Sync + Send>) -> Result<T, E>;
-    fn write<E>(&self, f: Box<dyn Fn(dyn WriteTx) -> Result<(), E> + Sync + Send>) -> Result<(), E>;
+    fn read<T, E>(&self, f: Box<dyn Fn(Box<dyn ReadTx>) -> Result<T, E>  + Sync + Send>) -> Result<T, E>;
+    fn write<E>(&self, f: Box<dyn Fn(Box<dyn WriteTx>) -> Result<(), E> + Sync + Send>) -> Result<(), E>;
 }
 
 pub trait ReadTx {
     fn key_exists(&self, key: &[u8]) -> bool;
     fn prefix_exists(&self, prefix: &[u8]) -> bool;
     fn get(&self, key: &[u8]) -> Option<Vec<u8>>;
-    fn prefix_scan(&self, prefix: &[u8]) -> dyn Iterator<Item=(Vec<u8>, Vec<u8>)>;
-    fn range_scan(&self, from: &[u8], to: &[u8]) -> dyn Iterator<Item=(Vec<u8>, Vec<u8>)>;
-    fn scan_all(&self) -> dyn Iterator<Item=(Vec<u8>, Vec<u8>)>;
+    fn prefix_scan(&self, prefix: &[u8]) -> Box<dyn Iterator<Item=(Vec<u8>, Vec<u8>)>>;
+    fn range_scan(&self, from: &[u8], to: &[u8]) -> Box<dyn Iterator<Item=(Vec<u8>, Vec<u8>)>>;
+    fn scan_all(&self) -> Box<dyn Iterator<Item=(Vec<u8>, Vec<u8>)>>;
 }
 
 pub trait WriteTx {
