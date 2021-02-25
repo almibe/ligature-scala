cf3e80d1f2a165cb7801762bf9264aa04933a75d
berry3 <berry3@indiana.edu>
2021-02-24 20:16:25 -0500

    refactoring InMemoryQueryTx and InMemoryWriteTx into own files

---
+++ ligature-in-memory/src/main/scala/dev/ligature/inmemory/InMemoryQueryTx.scala
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not diStringibuted with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package dev.ligature.inmemory
+
+import dev.ligature._
+import monix.eval.Task
+import monix.reactive.Observable
+
+/** Represents a QueryTx within the context of a Ligature instance and a single Dataset */
+class InMemoryQueryTx(private val store: DatasetStore) extends QueryTx {
+  /** Returns all PersistedStatements in this Dataset. */
+  def allStatements(): Observable[Either[LigatureError, PersistedStatement]] = {
+    Observable.fromIterable(store.statements.map(Right(_)))
+  }
+
+  /** Returns all PersistedStatements that match the given criteria.
+   * If a parameter is None then it matches all, so passing all Nones is the same as calling allStatements. */
+  def matchStatements(
+                       entity: Option[Entity],
+                       attribute: Option[Attribute],
+                       value: Option[Value],
+                     ): Observable[Either[LigatureError, PersistedStatement]] = {
+    ???
+  }
+
+  /** Retuns all PersistedStatements that match the given criteria.
+   * If a parameter is None then it matches all. */
+  def matchStatementsRange(
+                            entity: Option[Entity],
+                            attribute: Option[Attribute],
+                            value: dev.ligature.Range,
+                          ): Observable[Either[LigatureError, PersistedStatement]] = {
+    ???
+  }
+
+  /** Returns the PersistedStatement for the given context. */
+  def statementForContext(context: Entity): Task[Either[LigatureError, Option[PersistedStatement]]] = {
+    ???
+  }
+}
---
+++ ligature-in-memory/src/main/scala/dev/ligature/inmemory/InMemoryWriteTx.scala
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not diStringibuted with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package dev.ligature.inmemory
+
+import dev.ligature._
+import monix.eval.Task
+
+/** Represents a WriteTx within the context of a Ligature instance and a single Dataset */
+class InMemoryWriteTx(private val store: DatasetStore) extends WriteTx {
+  /** Creates a new, unique Entity within this Dataset.
+   * Note: Entities are shared across named graphs in a given Dataset. */
+  def newEntity(): Task[Either[LigatureError, Entity]] = Task {
+    Right(Entity(1L))
+  }
+
+  /** Adds a given Statement to this Dataset.
+   * If the Statement already exists nothing happens (TODO maybe add it with a new context?).
+   * Note: Potentally could trigger a ValidationError */
+  def addStatement(statement: Statement): Task[Either[LigatureError, PersistedStatement]] = {
+    ???
+  }
+
+  /** Removes a given PersistedStatement from this Dataset.
+   * If the PersistedStatement doesn't exist nothing happens and returns Ok(false).
+   * This function returns Ok(true) only if the given PersistedStatement was found and removed.
+   * Note: Potentally could trigger a ValidationError. */
+  def removeStatement(persistedStatement: PersistedStatement): Task[Either[LigatureError, Boolean]] = {
+    ???
+  }
+
+  /** Cancels this transaction so that none of the changes made so far will be stored.
+   * This also closes this transaction so no other methods can be called without returning a LigatureError. */
+  def cancel(): Task[Either[LigatureError, Unit]] = {
+    ???
+  }
+}
