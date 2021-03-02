30d9e2befdc318e1448695735555b270df47d474
berry3 <berry3@indiana.edu>
2021-03-02 18:18:10 -0500

    adding skeleton for testing atomic api, #27

--- slonky/src/test/kotlin/dev/ligature/slonky/SlonkySuite.kt
+++ slonky/src/test/kotlin/dev/ligature/slonky/SlonkySuite.kt
@@ -2,5 +2,131 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-package dev.ligature.slonky.app
+package dev.ligature.slonky
 
+import io.kotest.core.spec.style.FunSpec
+import io.vertx.core.Vertx
+import io.vertx.ext.web.client.WebClient
+
+class SlonkySuite: FunSpec() {
+    init {
+        val root = "http://localhost:4444/"
+
+//        test("Datasets should initially be empty") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty())
+//            TODO()
+//        }
+//
+//        test("Add Datasets") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Query Datasets w/ prefix") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Query Datasets w/ range") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Delete Datasets") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Statements in new Dataset should start empty") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Add Statements") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Match Statements") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Match Statements with ranges") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//
+//        test("Delete Statements") {
+//            main() //TODO should be in before
+//            val vertx = Vertx.vertx()
+//            val client = WebClient.create(vertx)
+//
+//            //TODO insert POSTs to add Datasets
+//
+//            val res = client.get("$root/").send().result()
+//            assert(res.bodyAsString().lines().isEmpty()) //TODO replace with shouldBe w/ values
+//            TODO()
+//        }
+//    }
+}
