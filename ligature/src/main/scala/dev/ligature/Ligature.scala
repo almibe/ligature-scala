9362f5e078ecd23236ef2fa6ee0990636e08dea0
berry3 <berry3@indiana.edu>
2021-03-28 18:42:39 -0400

    fixing compiler errors for Ligature.scala, #35

--- ligature/src/main/scala/dev/ligature/Ligature.scala
+++ ligature/src/main/scala/dev/ligature/Ligature.scala
@@ -5,6 +5,7 @@
 package dev.ligature
 
 import cats.effect.{Resource, IO}
+import fs2.Stream
 
 final case class Dataset private (val name: String) extends Ordered[Dataset] {
   private def copy(): Unit = ()
@@ -63,49 +64,49 @@ final case class Statement(val entity: Entity, val attribute: Attribute, val val
 final case class PersistedStatement(val statement: Statement, val context: Entity)
 
 trait Ligature {
-  def instance: Resource[Task, LigatureInstance]
+  def instance: Resource[IO, LigatureInstance]
 }
 
 /** A trait that all Ligature implementations implement. */
 trait LigatureInstance {
   /** Returns all Datasets in a Ligature instance. */
-  def allDatasets(): Observable[Either[LigatureError, Dataset]]
+  def allDatasets(): Stream[IO, Either[LigatureError, Dataset]]
 
   /** Check if a given Dataset exists. */
-  def datasetExists(dataset: Dataset): Task[Either[LigatureError, Boolean]]
+  def datasetExists(dataset: Dataset): IO[Either[LigatureError, Boolean]]
 
   /** Returns all Datasets in a Ligature instance that start with the given prefix. */
   def matchDatasetsPrefix(
                            prefix: String,
-                         ): Observable[Either[LigatureError, Dataset]]
+                         ): Stream[IO, Either[LigatureError, Dataset]]
 
   /** Returns all Datasets in a Ligature instance that are in a given range (inclusive, exclusive]. */
   def matchDatasetsRange(
                           start: String,
                           end: String,
-                        ): Observable[Either[LigatureError, Dataset]]
+                        ): Stream[IO, Either[LigatureError, Dataset]]
 
   /** Creates a dataset with the given name.
    * TODO should probably return its own error type { InvalidDataset, DatasetExists, CouldNotCreateDataset } */
-  def createDataset(dataset: Dataset): Task[Either[LigatureError, Unit]]
+  def createDataset(dataset: Dataset): IO[Either[LigatureError, Unit]]
 
   /** Deletes a dataset with the given name.
    * TODO should probably return its own error type { InvalidDataset, CouldNotDeleteDataset } */
-  def deleteDataset(dataset: Dataset): Task[Either[LigatureError, Unit]]
+  def deleteDataset(dataset: Dataset): IO[Either[LigatureError, Unit]]
 
   /** Initiazes a QueryTx
    * TODO should probably return its own error type CouldNotInitializeQueryTx */
-  def query(dataset: Dataset): Resource[Task, QueryTx]
+  def query(dataset: Dataset): Resource[IO, QueryTx]
 
   /** Initiazes a WriteTx
    * TODO should probably return its own error type CouldNotInitializeWriteTx */
-  def write(dataset: Dataset): Resource[Task, WriteTx]
+  def write(dataset: Dataset): Resource[IO, WriteTx]
 }
 
 /** Represents a QueryTx within the context of a Ligature instance and a single Dataset */
 trait QueryTx {
   /** Returns all PersistedStatements in this Dataset. */
-  def allStatements(): Observable[Either[LigatureError, PersistedStatement]]
+  def allStatements(): Stream[IO, Either[LigatureError, PersistedStatement]]
 
   /** Returns all PersistedStatements that match the given criteria.
    * If a parameter is None then it matches all, so passing all Nones is the same as calling allStatements. */
@@ -113,7 +114,7 @@ trait QueryTx {
                        entity: Option[Entity],
                        attribute: Option[Attribute],
                        value: Option[Value],
-                     ): Observable[Either[LigatureError, PersistedStatement]]
+                     ): Stream[IO, Either[LigatureError, PersistedStatement]]
 
   /** Retuns all PersistedStatements that match the given criteria.
    * If a parameter is None then it matches all. */
@@ -121,24 +122,24 @@ trait QueryTx {
                             entity: Option[Entity],
                             attribute: Option[Attribute],
                             value: Range,
-                          ): Observable[Either[LigatureError, PersistedStatement]]
+                          ): Stream[IO, Either[LigatureError, PersistedStatement]]
 
   /** Returns the PersistedStatement for the given context. */
   def statementForContext(
                            context: Entity,
-                         ): Task[Either[LigatureError, Option[PersistedStatement]]]
+                         ): IO[Either[LigatureError, Option[PersistedStatement]]]
 }
 
 /** Represents a WriteTx within the context of a Ligature instance and a single Dataset */
 trait WriteTx {
   /** Creates a new, unique Entity within this Dataset by combining a UUID and an optional prefix.
    * Note: Entities are shared across named graphs in a given Dataset. */
-  def newAnonymousEntity(prefix: String = ""): Task[Either[LigatureError, Entity]]
+  def newAnonymousEntity(prefix: String = ""): IO[Either[LigatureError, Entity]]
 
   /** Adds a given Statement to this Dataset.
    * If the Statement already exists nothing happens (TODO maybe add it with a new context?).
    * Note: Potentally could trigger a ValidationError */
-  def addStatement(statement: Statement): Task[Either[LigatureError, PersistedStatement]]
+  def addStatement(statement: Statement): IO[Either[LigatureError, PersistedStatement]]
 
   /** Removes a given PersistedStatement from this Dataset.
    * If the PersistedStatement doesn't exist nothing happens and returns Ok(false).
@@ -146,9 +147,9 @@ trait WriteTx {
    * Note: Potentally could trigger a ValidationError. */
   def removeStatement(
                        persistedStatement: PersistedStatement,
-                     ): Task[Either[LigatureError, Boolean]]
+                     ): IO[Either[LigatureError, Boolean]]
 
   /** Cancels this transaction so that none of the changes made so far will be stored.
    * This also closes this transaction so no other methods can be called without returning a LigatureError. */
-  def cancel(): Task[Either[LigatureError, Unit]]
+  def cancel(): IO[Either[LigatureError, Unit]]
 }
