f80554fc0f3eb2b7fcae8a69c469847e39e357e9
berry3 <berry3@indiana.edu>
2021-03-29 18:39:45 -0400

    starting work on rewriting ligature-in-memory, #37

--- ligature-in-memory/src/main/scala/dev/ligature/inmemory/InMemoryWriteTx.scala
+++ ligature-in-memory/src/main/scala/dev/ligature/inmemory/InMemoryWriteTx.scala
@@ -4,56 +4,57 @@
 
 package dev.ligature.inmemory
 
-import dev.ligature.*
-import java.util.*
-import kotlin.Result.Companion.success
+import cats.effect.IO
+import dev.ligature._
 
 /** Represents a WriteTx within the context of a Ligature instance and a single Dataset */
-class InMemoryWriteTx(private val store: DatasetStore) : WriteTx {
+class InMemoryWriteTx(private val store: DatasetStore) extends WriteTx {
     private var _isCanceled = false
     private var _newDatasetStore = store.copy()
 
     /** Creates a new, unique Entity within this Dataset.
      *  Note: Entities are shared across named graphs in a given Dataset. */
-    override suspend fun newAnonymousEntity(prefix: String): Result<Entity> {
-        val uuid = UUID.randomUUID()
-        return success(Entity(prefix + uuid))
+    def newEntity(): Task[Either[LigatureError, Entity]] = Task {
+        val counter = _newDatasetStore.counter + 1L
+        _newDatasetStore = _newDatasetStore.copy(counter = counter)
+        Right(Entity(counter))
     }
 
     /** Adds a given Statement to this Dataset.
      *  If the Statement already exists nothing happens (TODO maybe add it with a new context?).
      *  Note: Potentally could trigger a ValidationError */
-    override suspend fun addStatement(statement: Statement): Result<PersistedStatement> {
-        val context = newAnonymousEntity().getOrThrow()
-        val persistedStatement = PersistedStatement(statement, context)
-        _newDatasetStore = _newDatasetStore.copy(statements = _newDatasetStore.statements + persistedStatement)
-        return success(persistedStatement)
+    def addStatement(statement: Statement): Task[Either[LigatureError, PersistedStatement]] = Task {
+        val counter = _newDatasetStore.counter + 1L
+        val persistedStatement = PersistedStatement(statement, Entity(counter))
+        _newDatasetStore = _newDatasetStore.copy(counter = counter, statements = _newDatasetStore.statements + persistedStatement)
+        Right(persistedStatement)
     }
 
     /** Removes a given PersistedStatement from this Dataset.
      *  If the PersistedStatement doesn't exist nothing happens and returns Ok(false).
      *  This function returns Ok(true) only if the given PersistedStatement was found and removed.
      *  Note: Potentally could trigger a ValidationError. */
-    override suspend fun removeStatement(persistedStatement: PersistedStatement): Result<Boolean> =
+    def removeStatement(persistedStatement: PersistedStatement): Task[Either[LigatureError, Boolean]] = Task {
         if (_newDatasetStore.statements.contains(persistedStatement)) {
-            _newDatasetStore = _newDatasetStore.copy(statements = _newDatasetStore.statements.minus(persistedStatement))
-            success(true)
+            _newDatasetStore = _newDatasetStore.copy(statements = _newDatasetStore.statements.excl(persistedStatement))
+            Right(true)
         } else {
-            success(false)
+            Right(false)
         }
+    }
 
     /** Cancels this transaction so that none of the changes made so far will be stored.
      *  This also closes this transaction so no other methods can be called without returning a LigatureError. */
-    override suspend fun cancel(): Result<Unit> {
+    def cancel(): Task[Either[LigatureError, Unit]] = {
         _isCanceled = true
-        return success(Unit)
+        Task(Right(()))
     }
 
     /** Tracks if the given WriteTx has been canceled or not.
      *  Used in the release method of the Resource[Task, WriteTx]. */
-    fun isCanceled(): Boolean = _isCanceled
+    private[inmemory] def isCanceled(): Boolean = _isCanceled
 
     /** Returns the DatasetStore that has been modified by this WriteTx.
      *  Used in the release method of the Resource[Task, WriteTx]. */
-    fun newDatasetStore(): DatasetStore = _newDatasetStore
+    private[inmemory] def newDatasetStore(): DatasetStore = _newDatasetStore
 }
