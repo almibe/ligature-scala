10c19697fcee73372ec287311c49a5db9bf17812
berry3 <berry3@indiana.edu>
2021-04-18 22:29:20 -0400

    starting work on Lig serialization project, #50

---
+++ lig/src/test/kotlin/dev/ligature/lig/LigSuite.kt
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package dev.ligature.lig
+
+import dev.ligature.*
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class LigSuite : FunSpec() {
+    init {
+        val lig = Lig()
+
+        test("basic Statement with all Entities") {
+            val statement = Statement(Entity("e1"), Attribute("a1"), Entity("e2"), Entity("context"))
+            val lines = lig.write(listOf(statement).iterator())
+            val resStatements = lig.parse(lines)
+            listOf(statement) shouldBe listOf(resStatements)
+        }
+
+        test("list of Statements with Literal Values") {
+            val statements = listOf(
+                Statement(Entity("e1"), Attribute("a1"), Entity("e2"), Entity("context")),
+                Statement(Entity("e2"), Attribute("a2"), StringLiteral("string literal"), Entity("context2")),
+                Statement(Entity("e2"), Attribute("a3"), IntegerLiteral(Long.MAX_VALUE), Entity("context3")),
+                Statement(Entity("e3"), Attribute("a4"), FloatLiteral(7.5), Entity("context4"))
+            )
+            val lines = lig.write(statements.iterator())
+            val resStatements = lig.parse(lines)
+            listOf(statements) shouldBe listOf(resStatements)
+        }
+
+        test("parsing with wildcards") {
+            val textInput = "<e1> @<a2> 777 <e5>\n" +
+                    "_ @<a3> _ <e6>\n" +
+                    "_ _ \"Hello\" _\n" +
+                    "<e7> _ _ <e5>\n"
+            val expectedStatements = listOf(
+                Statement(Entity("e1"), Attribute("a2"), IntegerLiteral(777), Entity("e5")),
+                Statement(Entity("e1"), Attribute("a3"), IntegerLiteral(777), Entity("e6")),
+                Statement(Entity("e1"), Attribute("a3"), StringLiteral("Hello"), Entity("e6")),
+                Statement(Entity("e7"), Attribute("a3"), StringLiteral("Hello"), Entity("e5"))
+            )
+            val resStatements = lig.parse(textInput)
+            resStatements shouldBe expectedStatements
+        }
+    }
+}
