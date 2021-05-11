5a706013d454b538963a02ee5a9c8b2542e30f81
berry3 <berry3@indiana.edu>
2021-05-11 05:21:51 -0400

    using lig to parser StringLiterals in Wander + clean up, closes #79

--- wander/src/main/kotlin/dev/ligature/wander/lexer/Lexer.kt
+++ wander/src/main/kotlin/dev/ligature/wander/lexer/Lexer.kt
@@ -28,26 +28,6 @@ class Lexer {
         return Either.Right(tokens)
     }
 
-    private fun Char.isNewLine(): Boolean {
-        return this == '\n'
-    }
-
-    private fun Char.isString(): Boolean {
-        return this == '"'
-    }
-
-    private fun Char.isOperator(): Boolean =
-        when (this) {
-            '=' -> true //TODO add all other operators
-            else -> false
-        }
-
-    private fun Char.isIdentifier(): Boolean =
-        when (this) {
-            in 'a'..'z', in 'A'..'Z', '_' -> true
-            else -> false
-        }
-
     private fun readNewLine(rakkoon: Rakkoon): Either<LexerError, WanderToken> =
         when (rakkoon.peek()) {
             '\n' -> {
@@ -123,7 +103,11 @@ class Lexer {
     }
 
     private fun readString(rakkoon: Rakkoon): Either<LexerError, WanderToken> {
-        TODO("Use implementation from lig")
+        val initialOffset = rakkoon.currentOffset()
+        return when (val res = ligParser.parseStringLiteral(rakkoon)) {
+            is Either.Left -> Either.Left(LexerError("Could not parse StringLiteral.", rakkoon.currentOffset()))
+            is Either.Right -> Either.Right(WanderToken(initialOffset, StringPrimitive(res.value)))
+        }
     }
 
     private fun readToken(rakkoon: Rakkoon): Either<LexerError, WanderToken> {
@@ -137,28 +121,5 @@ class Lexer {
             in 'a'..'z', in 'A'..'Z', '_' -> readIdentifier(rakkoon)
             else -> return Either.Left(LexerError("Unexpected input.\n${rakkoon.remainingText()}", rakkoon.currentOffset()))
         }
-    }
-
-    //TODO remove after testing all primitives
-    private fun readPrimitive(rakkoon: Rakkoon): Either<WanderError, Primitive> {
-        val attributeRes = ligParser.parseAttribute(rakkoon)
-        if (attributeRes.isRight()) return attributeRes.map { AttributePrimitive(it) }.mapLeft { TODO() }
-
-//        val booleanRes = rakkoon.bite(booleanRule)
-//        if (booleanRes.isRight()) return booleanRes.map { BooleanPrimitive(it.toString().toBoolean()) }.mapLeft { TODO() }
-
-        val entityRes = ligParser.parseEntity(rakkoon)
-        if (entityRes.isRight()) return entityRes.map { EntityPrimitive(it) }.mapLeft { TODO() }
-
-        val floatRes = ligParser.parseFloatLiteral(rakkoon)
-        if (floatRes.isRight()) return floatRes.map { FloatPrimitive(it) }.mapLeft { TODO() }
-
-        val integerRes = ligParser.parseIntegerLiteral(rakkoon)
-        if (integerRes.isRight()) return integerRes.map { IntegerPrimitive(it) }.mapLeft { TODO() }
-
-        val stringRes = ligParser.parseStringLiteral(rakkoon)
-        if (stringRes.isRight()) return stringRes.map { StringPrimitive(it) }.mapLeft { TODO() }
-
-        TODO()
     }
 }
