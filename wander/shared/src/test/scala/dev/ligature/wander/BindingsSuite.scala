17918d4059a951071a39a87ea356dcd82c117fd3
berry3 <berry3@indiana.edu>
2022-02-18 20:17:16 -0500

    updating bindings tests, #194

--- wander/shared/src/test/scala/dev/ligature/wander/BindingsSuite.scala
+++ wander/shared/src/test/scala/dev/ligature/wander/BindingsSuite.scala
@@ -18,32 +18,27 @@ class BindingsSuite extends FunSuite {
 
   test("add single value and read") {
     val binding = Bindings()
-    binding.bind(identifier, value1)
+    val binding2 = binding.bindVariable(identifier, value1).getOrElse(???)
     val res = binding.read(identifier)
-    assertEquals(res, Right(value1))
-    intercept[Error] {
-      binding.read(identifier2)
-    }
+    val res2 = binding2.read(identifier)
+
+    assert(res.isLeft)
+    assert(binding.read(identifier2).isLeft)
+    assertEquals(res2, Right(value1))
+    assert(binding2.read(identifier2).isLeft)
   }
 
   test("test scoping") {
-    val binding = Bindings()
-    binding.bind(identifier, value1)
-    assertEquals(binding.read(identifier), Right(value1))
+    val bindings = Bindings()
+    val bindings2 = bindings.bindVariable(identifier, value1).getOrElse(???)
+    assertEquals(bindings2.read(identifier), Right(value1))
 
-    binding.addScope()
-    assertEquals(binding.read(identifier), Right(value1))
+    val bindings3 = bindings2.newScope()
+    assertEquals(bindings3.read(identifier), Right(value1))
 
-    binding.bind(identifier, value2)
-    binding.bind(identifier2, value3)
-    assertEquals(binding.read(identifier), Right(value2))
-    assertEquals(binding.read(identifier2), Right(value3))
-
-    binding.removeScope()
-    assertEquals(binding.read(identifier), Right(value1))
-
-    assert(binding.read(identifier2).isLeft)
-
-    assert(binding.removeScope().isLeft)
+    val bindings4 = bindings3.bindVariable(identifier, value2).getOrElse(???)
+    val bindings5 = bindings4.bindVariable(identifier2, value3).getOrElse(???)
+    assertEquals(bindings5.read(identifier), Right(value2))
+    assertEquals(bindings5.read(identifier2), Right(value3))
   }
 }
