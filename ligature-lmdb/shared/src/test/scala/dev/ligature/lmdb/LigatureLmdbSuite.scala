9bf0d9b2ac18c981f10f03a290d31693c6360b4d
berry3 <berry3@indiana.edu>
2022-06-01 02:04:05 -0400

    removing unused import

--- ligature-lmdb/shared/src/test/scala/dev/ligature/lmdb/LigatureLmdbSuite.scala
+++ ligature-lmdb/shared/src/test/scala/dev/ligature/lmdb/LigatureLmdbSuite.scala
@@ -4,8 +4,35 @@
 
 package dev.ligature.lmdb
 
+import dev.ligature.Ligature
 import dev.ligature.testsuite.LigatureTestSuite
+import java.nio.file._
+import java.io.File
 
-class LigatureLmdbSpec { // extends LigatureTestSuite() {
-  // override def createLigature = LmdbLigature()
+class LigatureLmdbSpec extends LigatureTestSuite() {
+  var path: Path = null
+  var ligatureInstance: Ligature = null
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    path = Files.createTempDirectory("LigatureLMDBTest")
+  }
+
+  override def afterEach(context: AfterEach): Unit = {
+    def deleteRecursively(file: File): Unit = {
+      if (file.isDirectory) {
+        file.listFiles.foreach(deleteRecursively)
+      }
+      if (file.exists && !file.delete) {
+        throw new Exception(s"Unable to delete ${file.getAbsolutePath}")
+      }
+    }
+
+    ligatureInstance.close().unsafeRunSync()
+    deleteRecursively(path.toFile())
+  }
+
+  override def createLigature = {
+    ligatureInstance = LmdbLigature(path.toFile())
+    ligatureInstance
+  }
 }
