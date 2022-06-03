105927e508833ec0bc1ac5374c9c6fe693adfcd6
berry3 <berry3@indiana.edu>
2022-06-03 02:46:13 -0400

    rewriting a couple of things to work better with Xodus Environments, #252

--- ligature-xodus/shared/src/main/scala/dev/ligature/xodus/XodusLigature.scala
+++ ligature-xodus/shared/src/main/scala/dev/ligature/xodus/XodusLigature.scala
@@ -9,42 +9,78 @@ import cats.effect.IO
 import fs2.Stream
 
 import scala.collection.immutable.TreeMap
+import scala.collection.mutable.ListBuffer
 import cats.effect.kernel.Resource
 import cats.effect.kernel.Ref
 
 import java.io.File
-import jetbrains.exodus.env.{EnvironmentConfig, Environments, ReadonlyTransaction, Transaction, TransactionalComputable}
+import jetbrains.exodus.env.{EnvironmentConfig, Environments, ReadonlyTransaction, Transaction, TransactionalComputable, Store, StoreConfig}
 import scala.jdk.CollectionConverters._
 
 final class XodusLigature(dbDirectory: File) extends Ligature {
   private val environment = Environments.newInstance(dbDirectory, new EnvironmentConfig())
+  setupStores()
 
-  enum Store(val storeName: String):
-    case countersStore extends Store("Counters")
-    case datasetToIdStore extends Store("DatasetToID")
-    case idToDatasetStore extends Store("IDToDataset")
-    case eavStore extends Store("EAV")
-    case evaStore extends Store("EVA")
-    case aevStore extends Store("AEV")
-    case aveStore extends Store("AVE")
-    case veaStore extends Store("VEA")
-    case vaeStore extends Store("VAE")
-    case identifierToIdStore extends Store("IdentifierToID")
-    case IdToIdentifierStore extends Store("IDToIdentifier")
-    case stringToIdStore extends Store("StringToID")
-    case IdToStringStore extends Store("IDToString")
-    case bytesToIdStore extends Store("BytesToID")
-    case IdToBytesStore extends Store("IDToBytes")
+  enum LigatureStore(val storeName: String):
+    case CountersStore extends LigatureStore("Counters")
+    case DatasetToIdStore extends LigatureStore("DatasetToID")
+    case IdToDatasetStore extends LigatureStore("IDToDataset")
+    case EAVStore extends LigatureStore("EAV")
+    case EVAStore extends LigatureStore("EVA")
+    case AEVStore extends LigatureStore("AEV")
+    case AVEStore extends LigatureStore("AVE")
+    case VEAStore extends LigatureStore("VEA")
+    case VAEStore extends LigatureStore("VAE")
+    case IdentifierToIdStore extends LigatureStore("IdentifierToID")
+    case IdToIdentifierStore extends LigatureStore("IDToIdentifier")
+    case StringToIdStore extends LigatureStore("StringToID")
+    case IdToStringStore extends LigatureStore("IDToString")
+    case BytesToIdStore extends LigatureStore("BytesToID")
+    case IdToBytesStore extends LigatureStore("IDToBytes")
+
+  /**
+   * This method is ran once at the start to make sure all Stores exist.
+   * This is done so that Stores can be opened in readonly mode.
+   * TODO: This method could probably check if the Environment exists and check the status of the Environment first.
+   */
+  private def setupStores(): Unit = {
+    val tc: TransactionalComputable[Unit] = tx => {
+      LigatureStore.values.foreach(openStore(tx, _))
+    }
+    environment.computeInTransaction(tc)
+  }
+
+  /**
+   * Used to uniformly open Stores.
+   * This is mainly so that StoreConfigs can be consistent.
+   * Right now all stores use the same config but that is likely to change.
+   */
+  private def openStore(tx: Transaction, store: LigatureStore): Store = {
+    environment.openStore(store.storeName, StoreConfig.WITHOUT_DUPLICATES, tx)
+  }
 
   /** Returns all Datasets in a Ligature instance. */
   override def allDatasets(): Stream[IO, Dataset] = Stream.emits {
-    val tc: TransactionalComputable[java.util.List[String]] = tx => environment.getAllStoreNames(tx)
-    environment.computeInReadonlyTransaction(tc).asScala.map(Dataset.fromString(_).getOrElse(???))
+    val tc: TransactionalComputable[List[String]] = tx => {
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      val datasetsCursor = datasetToIdStore.openCursor(tx)
+      val datasets: ListBuffer[String] = ListBuffer()
+      while (datasetsCursor.getNext) {
+        datasets.append(datasetsCursor.getKey.toString)
+      }
+      datasetsCursor.close() //TODO should use bracket
+      datasets.toList
+    }
+    environment.computeInReadonlyTransaction(tc).map(Dataset.fromString(_).getOrElse(???))
   }
 
   /** Check if a given Dataset exists. */
   override def datasetExists(dataset: Dataset): IO[Boolean] = IO {
-    val tc: TransactionalComputable[Boolean] = tx => environment.storeExists(dataset.name, tx)
+    val tc: TransactionalComputable[Boolean] = tx => {
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      ???
+      //datasetToIdStore.get(tx, )
+    }
     environment.computeInReadonlyTransaction(tc)
   }
 
@@ -66,13 +102,14 @@ final class XodusLigature(dbDirectory: File) extends Ligature {
     * error type { InvalidDataset, DatasetExists, CouldNotCreateDataset }
     */
   override def createDataset(dataset: Dataset): IO[Unit] = IO {
+    //TODO open DatasetToIdStore
+    //TODO check if Dataset exists if so return
+    //TODO if not open IdToDatasetStore and CounterStore
+    //TODO get next id
     val tc: TransactionalComputable[Unit] = tx => {
-      if (environment.storeExists(dataset.name, tx)) {
-        ()
-      } else {
-        PersistentEntityStores.newInstance(environment, dataset.name).close()
-        environment.getAllStoreNames(tx).forEach(println(_))
-      }
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      ???
+      //datasetToIdStore.get(tx, )
     }
     environment.computeInTransaction(tc)
   }
