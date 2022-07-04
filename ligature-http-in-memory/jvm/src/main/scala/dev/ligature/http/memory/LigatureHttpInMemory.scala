fd404ead063dbce2664a8f1dd7876121544816f5
Alex Michael Berry <alexmiberry@gmail.com>
2022-07-04 00:49:13 -0400

    fixing defaults for ligature-http-in-memory

--- ligature-http-in-memory/jvm/src/main/scala/dev/ligature/http/memory/LigatureHttpInMemory.scala
+++ ligature-http-in-memory/jvm/src/main/scala/dev/ligature/http/memory/LigatureHttpInMemory.scala
@@ -5,14 +5,19 @@
 package dev.ligature.http.memory
 
 import cats.effect.*
+import com.comcast.ip4s.Port
 import dev.ligature.inmemory.InMemoryLigature
-import dev.ligature.http.runLigature
+import dev.ligature.http.{AuthMode, runLigature}
 
-
+case class LigatureConfig(
+   authMode: AuthMode = AuthMode.None,
+   port: Port = Port.fromInt(4202).get,
+)
 
 object MainLigatureHttp extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
-    runLigature(InMemoryLigature(), args)
+    val config = LigatureConfig()
+    runLigature(InMemoryLigature(), config.authMode, config.port)
 }
 
 //} else {
