b8b63e25a5f50de053723434c9c8f4dbe9197d4f
Alex Michael Berry <alexmiberry@gmail.com>
2022-09-19 21:25:07 -0400

    rewriting XodusLigature in Kotlin, #289

--- ligature-xodus/src/jvmMain/kotlin/dev/ligature/xodus/XodusLigature.kt
+++ ligature-xodus/src/jvmMain/kotlin/dev/ligature/xodus/XodusLigature.kt
@@ -4,6 +4,8 @@
 
 package dev.ligature.xodus
 
+import arrow.core.Either.Left
+import arrow.core.Either.Right
 import dev.ligature.Dataset
 import dev.ligature.Identifier
 import dev.ligature.Ligature
@@ -28,6 +30,7 @@ import jetbrains.exodus.env.TransactionalExecutable
 import jetbrains.exodus.bindings.LongBinding
 import jetbrains.exodus.bindings.StringBinding
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 interface XodusOperations {
   fun openStore(tx: Transaction, store: LigatureStore): Store
@@ -118,207 +121,205 @@ class XodusLigature(private val dbDirectory: File): Ligature, XodusOperations {
   }
 
   /** Returns all Datasets in a Ligature instance. */
-  override fun allDatasets(): Flow<Dataset> = TODO() //Stream.emits {
-//    val tc: TransactionalComputable<List<String>> = tx => {
-//      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
-//      val datasetsCursor = datasetToIdStore.openCursor(tx)
-//      val datasets: ListBuffer<String> = ListBuffer()
-//      while (datasetsCursor.getNext)
-//        datasets.append(StringBinding.entryToString(datasetsCursor.getKey))
-//      datasetsCursor.close() // TODO should use bracket
-//      datasets.toList
-//    }
-//    environment.computeInReadonlyTransaction(tc).map(Dataset.fromString(_).getOrElse(???))
-//  }
+  override fun allDatasets(): Flow<Dataset> = flow {
+    val tc = TransactionalComputable<List<String>> { tx ->
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      val datasetsCursor = datasetToIdStore.openCursor(tx)
+      val datasets: MutableList<String> = mutableListOf()
+      while (datasetsCursor.next)
+        datasets.add(StringBinding.entryToString(datasetsCursor.key))
+      datasetsCursor.close() // TODO should use bracket
+      datasets
+    }
+    environment.computeInReadonlyTransaction(tc).map { Dataset.create(it) }.forEach {
+      when (it) {
+        is Right -> emit(it.value)
+        is Left  -> TODO()
+      }
+    }
+  }
 
-//  private def fetchDatasetID(dataset: Dataset): Option[ByteIterable] = {
-//    val tc: TransactionalComputable[Option[ByteIterable]] = tx => {
-//      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
-//      val res = datasetToIdStore.get(tx, StringBinding.stringToEntry(dataset.name))
-//      res match {
-//        case null                => None
-//        case bytes: ByteIterable => Some(bytes)
-//      }
-//    }
-//    environment.computeInReadonlyTransaction(tc)
-//  }
+  private fun fetchDatasetID(dataset: Dataset): ByteIterable? {
+    val tc = TransactionalComputable<ByteIterable?> { tx ->
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      datasetToIdStore.get(tx, StringBinding.stringToEntry(dataset.name))
+    }
+    return environment.computeInReadonlyTransaction(tc)
+  }
 
   /** Check if a given Dataset exists. */
-  override suspend fun datasetExists(dataset: Dataset): Boolean = TODO() //IO {
-//    fetchDatasetID(dataset).isDefined
-//  }
+  override suspend fun datasetExists(dataset: Dataset): Boolean =
+    fetchDatasetID(dataset) != null
 
   /** Returns all Datasets in a Ligature instance that start with the given
     * prefix.
     */
-  override fun matchDatasetsPrefix(prefix: String): Flow<Dataset> = TODO() //Stream.emits {
-//    val tc: TransactionalComputable[Seq[Dataset]] = tx => {
-//      val results: ArrayBuffer[Dataset] = ArrayBuffer()
-//      val cursor = openStore(tx, LigatureStore.DatasetToIdStore).openCursor(tx)
-//      val prefixBytes = StringBinding.stringToEntry(prefix)
-//      cursor.getSearchKeyRange(prefixBytes)
-//      var r = cursor.getKey
-//      var continue = true
-//      while (continue && r != null && StringBinding.entryToString(r).startsWith(prefix)) {
-//        results.append(Dataset.fromString(StringBinding.entryToString(r)).getOrElse(???))
-//        continue = cursor.getNext
-//        r = cursor.getKey
-//      }
-//      results.toSeq
-//    }
-//    environment.computeInReadonlyTransaction(tc)
-//  }
+  override fun matchDatasetsPrefix(prefix: String): Flow<Dataset> = flow {
+    val tc = TransactionalComputable<List<Dataset>> { tx ->
+      val results = mutableListOf<Dataset>()
+      val cursor = openStore(tx, LigatureStore.DatasetToIdStore).openCursor(tx)
+      val prefixBytes = StringBinding.stringToEntry(prefix)
+      cursor.getSearchKeyRange(prefixBytes)
+      var r: ByteIterable? = cursor.key
+      var `continue` = true
+      while (`continue` && r != null && StringBinding.entryToString(r).startsWith(prefix)) {
+        when (val ds = Dataset.create(StringBinding.entryToString(r))) {
+          is Right -> {
+            results.add(ds.value)
+            `continue` = cursor.next
+            r = cursor.key
+          }
+          is Left -> {
+            TODO()
+          }
+        }
+      }
+      results
+    }
+    environment.computeInReadonlyTransaction(tc).forEach { emit(it) }
+  }
 
   /** Returns all Datasets in a Ligature instance that are in a given range
     * (inclusive, exclusive].
     */
-  override fun matchDatasetsRange(start: String, end: String): Flow<Dataset> = TODO() //Stream.emits {
-//    val tc: TransactionalComputable[Seq[Dataset]] = tx => {
-//      val results: ArrayBuffer[Dataset] = ArrayBuffer()
-//      val cursor = openStore(tx, LigatureStore.DatasetToIdStore).openCursor(tx)
-//      val prefixBytes = StringBinding.stringToEntry(start)
-//      cursor.getSearchKeyRange(prefixBytes)
-//      var r = cursor.getKey
-//      var continue = true
-//      while (continue && r != null && stringInRange(StringBinding.entryToString(r), start, end)) {
-//        results.append(Dataset.fromString(StringBinding.entryToString(r)).getOrElse(???))
-//        continue = cursor.getNext
-//        r = cursor.getKey
-//      }
-//      results.toSeq
-//    }
-//    environment.computeInReadonlyTransaction(tc)
-//  }
+  override fun matchDatasetsRange(start: String, end: String): Flow<Dataset> = flow {
+    val tc = TransactionalComputable<List<Dataset>> { tx ->
+      val results = mutableListOf<Dataset>()
+      val cursor = openStore(tx, LigatureStore.DatasetToIdStore).openCursor(tx)
+      val prefixBytes = StringBinding.stringToEntry(start)
+      cursor.getSearchKeyRange(prefixBytes)
+      var r: ByteIterable? = cursor.key
+      var `continue` = true
+      while (`continue` && r != null && stringInRange(StringBinding.entryToString(r), start, end)) {
+        when (val ds = Dataset.create(StringBinding.entryToString(r))) {
+          is Right -> {
+            results.add(ds.value)
+            `continue` = cursor.next
+            r = cursor.key
+          }
+          is Left -> {
+            TODO()
+          }
+        }
+      }
+      results
+    }
+    environment.computeInReadonlyTransaction(tc)
+  }
 
-//  private def stringInRange(toTest: String, start: String, end: String): Boolean =
-//    toTest.compareTo(start) >= 0 && toTest.compareTo(end) < 0
+  private fun stringInRange(toTest: String, start: String, end: String): Boolean =
+    toTest >= start && toTest < end
 
   /** Creates a dataset with the given name. TODO should probably return its own
     * error type { InvalidDataset, DatasetExists, CouldNotCreateDataset }
     */
-  override suspend fun createDataset(dataset: Dataset): Unit = TODO() //IO {
-//    val tc: TransactionalExecutable = tx => {
-//      val nameEntry = StringBinding.stringToEntry(dataset.name)
-//      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
-//      val datasetExists = datasetToIdStore.get(tx, nameEntry)
-//      if (datasetExists == null) {
-//        val nextId = nextID(tx)
-//        val idToDatasetStore = openStore(tx, LigatureStore.IdToDatasetStore)
-//        datasetToIdStore.put(tx, nameEntry, nextId)
-//        idToDatasetStore.putRight(tx, nextId, nameEntry)
-//        tx.commit()
-//      } else {
-//        ()
-//      }
-//    }
-//    environment.executeInTransaction(tc)
-//  }
+  override suspend fun createDataset(dataset: Dataset) {
+    val tc = TransactionalExecutable { tx ->
+      val nameEntry = StringBinding.stringToEntry(dataset.name)
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      val datasetExists = datasetToIdStore.get(tx, nameEntry)
+      if (datasetExists == null) {
+        val nextId = nextID(tx)
+        val idToDatasetStore = openStore(tx, LigatureStore.IdToDatasetStore)
+        datasetToIdStore.put(tx, nameEntry, nextId)
+        idToDatasetStore.putRight(tx, nextId, nameEntry)
+        tx.commit()
+      }
+    }
+    environment.executeInTransaction(tc)
+  }
 
   /** Deletes a dataset with the given name. TODO should probably return its own
     * error type { InvalidDataset, CouldNotDeleteDataset }
     */
-  override suspend fun deleteDataset(dataset: Dataset): Unit = TODO() //IO {
-//    val tc: TransactionalExecutable = tx => {
-//      val nameEntry = StringBinding.stringToEntry(dataset.name)
-//      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
-//      val datasetId = datasetToIdStore.get(tx, nameEntry)
-//      if (datasetId != null) {
-//        val idToDatasetStore = openStore(tx, LigatureStore.IdToDatasetStore)
-//        datasetToIdStore.delete(tx, nameEntry)
-//        idToDatasetStore.delete(tx, datasetId)
-//        deleteAllStatements(tx, datasetId)
-//        tx.commit()
-//      } else {
-//        ()
-//      }
-//    }
-//    environment.executeInTransaction(tc)
-//  }
+  override suspend fun deleteDataset(dataset: Dataset) {
+    val tc = TransactionalExecutable { tx ->
+      val nameEntry = StringBinding.stringToEntry(dataset.name)
+      val datasetToIdStore = openStore(tx, LigatureStore.DatasetToIdStore)
+      val datasetId = datasetToIdStore.get(tx, nameEntry)
+      if (datasetId != null) {
+        val idToDatasetStore = openStore(tx, LigatureStore.IdToDatasetStore)
+        datasetToIdStore.delete(tx, nameEntry)
+        idToDatasetStore.delete(tx, datasetId)
+        deleteAllStatements(tx, datasetId)
+        tx.commit()
+      }
+    }
+    environment.executeInTransaction(tc)
+  }
 
-  /** This method iterates through all of the index stores and removes all Statements
+  /** This method iterates through all the index stores and removes all Statements
     * in a given Dataset.
     * TODO this method also needs to clean up all Identifiers, Strings, and Bytes
     */
-//  private def deleteAllStatements(tx: Transaction, datasetId: ByteIterable) =
-//    val stores = List(
-//      LigatureStore.EAVStore,
-//      LigatureStore.EVAStore,
-//      LigatureStore.AEVStore,
-//      LigatureStore.AVEStore,
-//      LigatureStore.VEAStore,
-//      LigatureStore.VAEStore,
-//      LigatureStore.IdentifierToIdStore,
-//      LigatureStore.IdToIdentifierStore,
-//      LigatureStore.StringToIdStore,
-//      LigatureStore.IdToStringStore,
-//      LigatureStore.BytesToIdStore,
-//      LigatureStore.IdToBytesStore
-//    )
-//    stores.foreach { store =>
-//      val cursor = openStore(tx, store).openCursor(tx)
-//      var continue = cursor.getSearchKeyRange(datasetId) != null
-//      while (continue) {
-//        val key = cursor.getKey
-//        if (key.subIterable(0, 8) == datasetId) {
-//          cursor.deleteCurrent()
-//          continue = cursor.getNext
-//        } else {
-//          continue = false
-//        }
-//      }
-//    }
+  private fun deleteAllStatements(tx: Transaction, datasetId: ByteIterable) {
+    val stores = listOf(
+      LigatureStore.EAVStore,
+      LigatureStore.EVAStore,
+      LigatureStore.AEVStore,
+      LigatureStore.AVEStore,
+      LigatureStore.VEAStore,
+      LigatureStore.VAEStore,
+      LigatureStore.IdentifierToIdStore,
+      LigatureStore.IdToIdentifierStore,
+      LigatureStore.StringToIdStore,
+      LigatureStore.IdToStringStore,
+      LigatureStore.BytesToIdStore,
+      LigatureStore.IdToBytesStore
+    )
+    stores.forEach { store ->
+      val cursor = openStore(tx, store).openCursor(tx)
+      var `continue` = cursor.getSearchKeyRange(datasetId) != null
+      while (`continue`) {
+        val key = cursor.key
+        `continue` = if (key.subIterable(0, 8) == datasetId) {
+          cursor.deleteCurrent()
+          cursor.next
+        } else {
+          false
+        }
+      }
+    }
+  }
 
   /** Initializes a QueryTx TODO should probably return its own error type
     * CouldNotInitializeQueryTx
     */
-  override suspend fun <T>query(dataset: Dataset, fn: suspend (QueryTx) -> T): T = TODO()
-//    IO {
-//      environment.beginReadonlyTransaction()
-//    }.bracket { tx =>
-//      IO.defer {
-//        fetchDatasetID(dataset) match {
-//          case None => ???
-//          case Some(datasetId) =>
-//            val queryTx = XodusQueryTx(tx, this, datasetId)
-//            fn(queryTx)
-//        }
-//      }
-//    } { tx =>
-//      IO.defer {
-//        if (!tx.isFinished) {
-//          tx.abort()
-//        }
-//        IO.unit
-//      }
-//    }
+  //TODO probably use arrow's Resource here?
+  override suspend fun <T>query(dataset: Dataset, fn: suspend (QueryTx) -> T): T {
+    val tx = environment.beginReadonlyTransaction()
+    val res = when (val datasetId = fetchDatasetID(dataset)) {
+      null -> TODO()
+      else -> {
+        val queryTx = XodusQueryTx(tx, this, datasetId)
+        fn(queryTx)
+      }
+    }
+    if (!tx.isFinished) {
+      tx.abort()
+    }
+    return res
+  }
 
   /** Initializes a WriteTx TODO should probably return its own error type
     * CouldNotInitializeWriteTx
     */
-  // TODO try rewriting to use bracket, like query does
-  override suspend fun <T>write(dataset: Dataset, fn: suspend (WriteTx) -> T): T = TODO()
-//    IO { // TODO rewrite with TransactionComputable
-//      environment.beginTransaction()
-//    }.bracket { tx =>
-//      IO.defer {
-//        fetchDatasetID(dataset) match {
-//          case None => ???
-//          case Some(datasetId) =>
-//            val writeTx = XodusWriteTx(tx, this, datasetId)
-//            fn(writeTx)
-//        }
-//      }
-//    } { tx =>
-//      IO.defer {
-//        if (!tx.isFinished) {
-//          tx.commit()
-//        }
-//        IO.unit
-//      }
-//    }
+  //TODO probably use arrow's Resource here?
+  override suspend fun <T>write(dataset: Dataset, fn: suspend (WriteTx) -> T): T {
+    val tx = environment.beginTransaction()
+    val res = when(val datasetId = fetchDatasetID(dataset)) {
+      null -> TODO()
+      else -> {
+        val writeTx = XodusWriteTx(tx, this, datasetId)
+        fn(writeTx)
+      }
+    }
+    if (!tx.isFinished) {
+      tx.commit()
+    }
+    return res
+  }
 
-  override suspend fun close(): Unit = TODO() //IO {
-//    environment.close()
-//    ()
-//  }
+  override suspend fun close(): Unit =
+    environment.close()
 }
