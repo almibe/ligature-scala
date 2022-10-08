de29f3ef53d34efc6b09da947b0a8bb2f3ce97cb
Alex Michael Berry <alexmiberry@gmail.com>
2022-10-08 02:23:30 -0400

    updating ligature-http to use new wander api

--- ligature-http/src/jvmMain/kotlin/dev/ligature/http/Handles.kt
+++ ligature-http/src/jvmMain/kotlin/dev/ligature/http/Handles.kt
@@ -9,8 +9,8 @@ import arrow.core.Either.Right
 import dev.ligature.Dataset
 import dev.ligature.Ligature
 import dev.ligature.lig.writeStatement
+import dev.ligature.wander.interpreter.write
 import dev.ligature.wander.library.common
-import dev.ligature.wander.library.datasetQueryBindings
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -102,9 +102,10 @@ class Handlers(private val ligature: Ligature) {
     when (val dataset = Dataset.create(datasetName)) {
       is Right -> {
         val script = call.receiveText()
-        val bindings = datasetQueryBindings(dataset.value)
+        //TODO fix bindings below
+        val bindings = common() //datasetQueryBindings(dataset.value)
         when (val res = dev.ligature.wander.run(script, bindings)) {
-          is Right -> call.respondText(res.value.printResult())
+          is Right -> call.respondText(write(res.value))
           is Left -> TODO("Return error message from running script")
         }
       }
