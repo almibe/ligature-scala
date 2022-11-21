0993c483b39952a3822964e0a6c689e89760bbaa
Alex Michael Berry <alexmiberry@gmail.com>
2022-11-21 01:56:32 -0500

    adding buildSrc back, #379

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins { `kotlin-dsl` }
+
+repositories { gradlePluginPortal() }
+
+dependencies {
+  implementation("org.jetbrains.kotlin.multiplatform:org.jetbrains.kotlin.multiplatform.gradle.plugin:1.7.21")
+  //implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
+}
---
+++ buildSrc/src/main/kotlin/dev.ligature.kotlin-library-conventions.gradle.kts
@@ -0,0 +1,1 @@
+plugins { id("dev.ligature.kotlin-common-conventions") }
