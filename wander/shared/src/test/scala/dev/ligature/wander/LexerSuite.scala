b4f2f60b4f5e41f50937a87878fd7074604ab6c2
Alex Michael Berry <alexmiberry@gmail.com>
2023-05-06 18:32:08 -0400

    adding LexerSuite, #410

--- wander/shared/src/test/scala/dev/ligature/wander/LexerSuite.scala
+++ wander/shared/src/test/scala/dev/ligature/wander/LexerSuite.scala
@@ -9,15 +9,42 @@ import dev.ligature.{Identifier, StringLiteral}
 import munit.FunSuite
 
 class LexerSuite extends FunSuite {
-  test("") {
-    // val binding = Bindings()
-    // val binding2 = binding.bindVariable(identifier, value1).getOrElse(???)
-    // val res = binding.read(identifier)
-    // val res2 = binding2.read(identifier)
+  def check(script: String, tokens: Either[ScriptError, Seq[Token]]) =
+    assertEquals(tokenize(script), tokens)
+  def ident(identifier: String): Token =
+    Identifier.fromString(identifier) match
+      case Left(value) => ??? //just crash
+      case Right(value) => Token.Identifier(value)
+  val sp = Token.Spaces(" ")
 
-    // assert(res.isLeft)
-    // assert(binding.read(identifier2).isLeft)
-    // assertEquals(res2, Right(value1))
-    // assert(binding2.read(identifier2).isLeft)
+  test("tokenize booleans") {
+    val script = "true false"
+    val tokens = Right(Seq(Token.BooleanLiteral(true), sp, Token.BooleanLiteral(false)))
+    check(script, tokens)
+  }
+  test("tokenize Identifiers") {
+    val script = "<hello><123><_4><https://ligature.dev>"
+    val tokens = Right(Seq(ident("hello"), ident("123"), ident("_4"), ident("https://ligature.dev")))
+    check(script, tokens)
+  }
+  test("tokenize Integers") {
+    val script = "123 0 -123"
+    val tokens = Right(Seq(Token.IntegerLiteral(123), sp, Token.IntegerLiteral(0), sp, Token.IntegerLiteral(-123)))
+    check(script, tokens)
+  }
+  test("tokenize Strings") {
+    val script = "\"hello, world!\""
+    val tokens = Right(Seq(Token.StringLiteral("hello, world!")))
+    check(script, tokens)
+  }
+  test("tokenize Names") {
+    val script = "hello world _test also_a_321test123"
+    val tokens = Right(Seq(Token.Name("hello"), sp, Token.Name("world"), sp, Token.Name("_test"), sp, Token.Name("also_a_321test123")))
+    check(script, tokens)
+  }
+  test("tokenize symbols") {
+    val script = "{}:()\n\r\n->if elsif else = let --test"
+    val tokens = Right(Seq(Token.OpenBrace, Token.CloseBrace, Token.Colon, Token.OpenParen, Token.CloseParen, Token.NewLine, Token.NewLine,
+    Token.Arrow, Token.IfKeyword, sp, Token.ElsifKeyword, sp, Token.ElseKeyword, sp, Token.EqualSign, sp, Token.LetKeyword, sp, Token.Comment))
   }
 }
