0adfe0cf30970e752e794dfc431518511c9dbed1
Alex Michael Berry <alexmiberry@gmail.com>
2024-11-21 04:46:39 -0500

    moving lib files around + clean up

--- ligature/src/main/scala/dev/ligature/wander/lib/Statement.scala
+++ ligature/src/main/scala/dev/ligature/wander/lib/Statement.scala
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package dev.ligature.wander.modules
+package dev.ligature.wander.lib
 
 // import dev.ligature.wander.LigatureValue
 // import dev.ligature.wander.{LigatureValue, Field, HostFunction, TaggedField, Tag}
